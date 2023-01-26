8d51ca51b4dec3536134804b67f46e5c1e8754dc
jhan <jhan.perez@correounivalle.edu.co>
2023-01-26 15:10:42 -0500

    try with gitignore

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ app = create_app()
 CORS(app)
 
 if __name__ == '__main__':
-    port = os.getenv('PORT', default=8000)
+    port = os.getenv('PORT', default=3001)
     app.run(host="0.0.0.0", port=port)
